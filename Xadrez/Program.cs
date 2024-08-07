9fdf939732acdbb7927288758c2c4d528987a5cc
cleber-gomes <cleberjunior155@outlook.com>
2024-08-06 21:20:13 -0300

    Implementando a logica de xequemate

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -41,6 +41,8 @@ namespace Xadrez
                         Console.ReadLine();
                     }
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
